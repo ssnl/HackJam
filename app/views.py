0ace26719f30d76a66ae61a72e3536619ec4f4cd
S_sn <S_sn@airbears2-10-142-33-233.airbears2.1918.berkeley.edu>
2014-03-01 16:15:58 -0800

    electric imp remote control

--- app/views.py
+++ app/views.py
@@ -10,17 +10,57 @@ def index():
 </head>
 <body>
 <script type="text/javascript">
-    function showText(){
+    // Let's create the iFrame used to send our data
+    var iframe = document.createElement("iframe");
+    iframe.name = "myTarget";
+
+    // Next, attach the iFrame to the main document
+    window.addEventListener("load", function () {
+      iframe.style.display = "none";
+      document.body.appendChild(iframe);
+    });
+
+    // This is the function used to actually send the data
+    // It takes one parameter, which is an object populated with key/value pairs.
+    function sendData(data) {
+      var name,
+          form = document.createElement("form"),
+          node = document.createElement("input");
+
+      // Define what should happen when the response is loaded
+      iframe.addEventListener("load", function () {
+        alert("Yeah! Data sent.");
+      });
+
+      form.action = "https://agent.electricimp.com/B1iqTo1VR_He";
+      form.target = iframe.name;
+
+      for(name in data) {
+        node.name  = name;
+        node.value = data[name].toString();
+        form.appendChild(node.cloneNode());
+      }
+
+      // To be sent, the form needs to be attached to the main document.
+      form.style.display = "none";
+      document.body.appendChild(form);
+
+      form.submit();
+
+      // But once the form is sent, it's useless to keep it.
+      document.body.removeChild(form);
+    }
+    function sendRequest(){
        var value = document.getElementById('options').value;
-       if(value != ""){
-            document.getElementById('show').innerHTML = value;
-            document.getElementById('show').style.display = "block";
+       if(value !== ""){
+            sendData({led: parseInt(value)});
        }
     }
 </script>
-    <select id="options" onchange="showText()">
+    <select id="options" onchange="sendRequest()">
+        <option value="">please choose</option>
 """
-    for i in range(1, 37):
+    for i in range(0, 36):
         s += "\no        <option value=\"{0}\">{0}</option>".format(i)
     s += """
     </select>
